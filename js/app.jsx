d8fa8541739bf341e4841e4e4eb3fc821b9d65ce
Shing Lyu <shing.lyu@gmail.com>
2014-12-26 14:23:30 +0800

    Hooked up with remote data source

--- js/app.jsx
+++ js/app.jsx
@@ -1,10 +1,10 @@
-var SearchInput = React.createClass({
+var SearchForm= React.createClass({
   render: function() {
     return (
-      <div>
-        <input type="text" id="searchInput"/>
-        <button id="searchSubmit">Search</button>
-      </div>
+      <form onSubmit={this.props.onSubmit}>
+        <input type="text" id="searchInput" defaultValue={this.props.query} />
+        <button type="submit" id="searchSubmit">Search</button>
+      </form>
     )
   }
 });
@@ -30,7 +30,8 @@ var CaseverListItem = React.createClass({
 
 var CaseverList = React.createClass({
   render: function() {
-    var casevers = this.props.casevers.map(function(casever){
+    //can use the casevers.meta
+    var casevers = this.props.casevers.objects.map(function(casever){
       return (<CaseverListItem casever={casever} />)
     })
 
@@ -43,21 +44,70 @@ var CaseverList = React.createClass({
 });
 
 var SearchableCaseverList = React.createClass({
+  loadCasevers: function() {
+    console.log("hi")
+    $.ajax({
+      url: buildQueryUrl(this.props.url, this.state.query),
+      dataType: 'jsonp',
+
+      success: function(data) {
+        this.setState({query: this.state.query, casevers: data});
+      }.bind(this),
+
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+
+  },
+
+  getInitialState: function() {
+    return {query: "product:\"Firefox OS\"", casevers: {meta:{}, objects: [{status:"Loading..."}]}};
+  },
+
+  componentDidMount: function() {
+    this.loadCasevers();
+  },
+
+  handleSearch: function() {
+    this.loadCasevers()
+  },
+
   render: function() {
     return (
       <div>
-        <SearchInput />
-        <CaseverList casevers={this.props.casevers}/>
+        <SearchForm query={this.state.query} onSubmit={this.handleSearch}/>
+        <CaseverList casevers={this.state.casevers}/>
       </div>
     )
   }
 })
 
+function parseQuery(query) {
+  var queries = query.split(" ")
+}
+function buildQueryUrl(url, queryStrings) {
+  //TODO: parse and transform query to tastypie filters
+  var queryUrl = url + "?"
+  //var queryStrings = parseQuery(query);
+  for (qs of queryStrings) {
+    queryUrl += ("&" + qs)
+  }
+
+  //var queryUrl = url + "?order_by=modified_on"
+  return queryUrl;
+}
+
 var mockCasevers = [
   {"status": "active", "name": "Foobar!", "productversion": "Firefox OS 2.2"},
   {"status": "draft", "name": "Foobarbar!", "productversion": "Firefox OS 2.1"}
-]
+];
+
+var apiUrl="https://moztrap.mozilla.org/api/v1/caseversion/";
+//use jsonp to overcome CORS
+//var jsonpApiUrl = "http://jsonp.nodejitsu.com/?callback=&url=" + apiUrl 
+
 React.render(
-  <SearchableCaseverList casevers={mockCasevers} />,
+  <SearchableCaseverList casevers={mockCasevers} url={apiUrl+ "?limit=20"}/>,
   document.getElementById("content")
 );
